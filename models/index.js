f27d81f0a2124b344f8d2471578604ffafd196b5
vjtolliver <vj.tolliver@gmail.com>
2023-05-07 17:01:52 -0700

    added models and seed data

---
+++ models/index.js
@@ -0,0 +1,32 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comments = require('./Comments');
+
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  
+  Post.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+
+  Post.hasMany(Comments, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+  });
+
+  Comments.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+
+  User.hasMany(Comments, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+
+  Comments.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+
+module.exports = { User, Post };
